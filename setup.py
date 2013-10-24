ffa79470a04513d73e93d898bb586c6f8e9dcffb
Eduard Carreras <ecarreras@gisce.net>
2013-10-24 18:45:25 +0200

    Bump to v0.1.0-alpha

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+"""
+Empowering
+======
+
+Python library for the Empowering REST API.
+
+:copyright: (c) 2013 by GISCE-TI, S.L., see AUTHORS for more details.
+:license: MIT, see LICENSE for more details.
+"""
+from setuptools import setup, find_packages
+
+tests_require = [
+    'Flask'
+]
+
+install_requires = [
+    'libsaas',
+]
+
+setup(
+    name='empowering',
+    version='0.1.0-alpha',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    url='http://code.gisce.net/empowering',
+    description='Python library for the Empowering REST API.',
+    long_description=__doc__,
+    license='MIT',
+    packages=find_packages(exclude=['tests', 'test']),
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    test_suite='nose.collector',
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
