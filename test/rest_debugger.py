148a5601963153cd32b22204d60fb6e38620cab9
Eduard Carreras <ecarreras@gisce.net>
2013-11-06 17:05:15 +0100

    Add similar behaviour on our test server

--- test/rest_debugger.py
+++ test/rest_debugger.py
@@ -1,4 +1,7 @@
 from flask import Flask, request
+from hashlib import sha1
+import json
+from uuid import uuid1
 
 app = Flask(__name__)
 
@@ -6,15 +9,27 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''}, methods=['GET', 'POST', 'PUT', 'PATCH'])
 @app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'PATCH'])
 def main(path):
-    print 'Path requested: %s' % path
+    print 'Path requested: %s %s' % (request.method, path)
     for key, value in request.headers:
         print "%s: %s" % (key, value)
     if request.json:
         print 'JSON: %s' % request.json
     else:
         print 'DATA: %s' % request.data
-    return "200"
-
+    if request.method in ('PATCH', 'POST') and request.json:
+        if isinstance(request.json, list):
+            res = []
+            for item in request.json:
+                res.append({
+                    'etag': sha1(str(uuid1())).hexdigest(),
+                    'orig': request.json
+                })
+        else:
+            res = {'etag': sha1(str(uuid1())).hexdigest(),
+                    'orig': request.json}
+        print "======>"
+        print "%s" % res
+        return json.dumps(res)
 
 if __name__ == '__main__':
     app.run(debug=True)
