cab8f80823c0c488866dc586c869070b401b5c9e
Eduard Carreras <ecarreras@gisce.net>
2014-03-31 17:37:25 +0200

    Bump to v0.6.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ install_requires = [
 
 setup(
     name='empowering',
-    version='0.6.0',
+    version='0.6.1',
     author='GISCE-TI, S.L.',
     author_email='devel@gisce.net',
     url='http://code.gisce.net/empowering',
