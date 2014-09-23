ee447b4ff07c0c3f6d5b124cb4f06b1083cb332b
Eduard Carreras <ecarreras@gisce.net>
2014-09-23 14:26:37 +0200

    Fail unitary

--- test/amon.py
+++ test/amon.py
@@ -486,21 +486,24 @@ def push_contracts(contracts_id):
     if not isinstance(contracts_id, (list, tuple)):
         contracts_id = [contracts_id]
     for cid in contracts_id:
-        pol = O.GiscedataPolissa.read(cid, ['modcontractuals_ids', 'name'])
-        upd = []
-        first = True
-        for modcon_id in reversed(pol['modcontractuals_ids']):
-            amon_data = amon.contract_to_amon(cid, {'modcon_id': modcon_id})[0]
-            if first:
-                upd.append(em.contracts().create(amon_data))
-                first = False
-            else:
-                etag = upd[-1]['_etag']
-                upd.append(em.contract(pol['name']).update(amon_data, etag))
-        res.append(upd[-1])
-        for idx, resp in enumerate(res):
-            pol_id = [contracts_id[idx]]
-            update_etag.delay(pol_id, resp)
+        try:
+            pol = O.GiscedataPolissa.read(cid, ['modcontractuals_ids', 'name'])
+            upd = []
+            first = True
+            for modcon_id in reversed(pol['modcontractuals_ids']):
+                amon_data = amon.contract_to_amon(cid, {'modcon_id': modcon_id})[0]
+                if first:
+                    upd.append(em.contracts().create(amon_data))
+                    first = False
+                else:
+                    etag = upd[-1]['_etag']
+                    upd.append(em.contract(pol['name']).update(amon_data, etag))
+            res.append(upd[-1])
+            for idx, resp in enumerate(res):
+                pol_id = [contracts_id[idx]]
+                update_etag.delay(pol_id, resp)
+        except:
+            push_contracts(cid)
 
 
 @job('etag', connection=Redis())
