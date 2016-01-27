ca3953c5d3b099b88d534a7124a63357435f1d9a
Eduard Carreras <ecarreras@gmail.com>
2016-01-27 13:58:28 +0100

    Update utils.py

--- empowering/utils.py
+++ empowering/utils.py
@@ -9,6 +9,17 @@ parser = DateTimeParser()
 times.parse = parser.parse_iso
 
 
+def fix_ssl_verify():
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        # Legacy Python that doesn't verify HTTPS certificates by default
+        pass
+    else:
+        # Handle target environment that doesn't support HTTPS verification
+        ssl._create_default_https_context = _create_unverified_https_context
+
+
 def remove_none(struct, context=None):
     if not context:
         context = {}
